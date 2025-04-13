88ae8e0854af13df78250118a537ba94fe58a473
metndurn <mtndrn123@gmail.com>
2025-04-13 23:49:17 +0300

    fluent api eklendi

---
+++ ApiProjectKampi.WebApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,22 @@
+﻿using ApiProjectKampi.WebApi.Entities;
+using FluentValidation;
+
+namespace ApiProjectKampi.WebApi.ValidationRules
+{
+	/*FluentValidation, C# uygulamalarında nesnelerin doğrulama kurallarını tanımlamak için kullanılan bir kütüphanedir.*/
+	public class ProductValidator : AbstractValidator<Product>
+	{
+		public ProductValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Ürün adı boş olamaz.!");
+			RuleFor(x => x.Name).MinimumLength(2).WithMessage("Ürün adı en az 2 karakter olmalıdır.!");
+			RuleFor(x => x.Name).MaximumLength(50).WithMessage("Ürün adı en fazla 50 karakter olmalıdır.!");
+
+			RuleFor(x => x.Price).NotEmpty().WithMessage("Ürün fiyatı boş olamaz.!")
+			.LessThan(0).WithMessage("Ürün fiyatı negatif olamaz.!")
+			.GreaterThan(2000).WithMessage("Ürün fiyati bu kadar yüksek olamaz girdiğiniz değeri kontrol edin.!");
+
+			RuleFor(x => x.Description).NotEmpty().WithMessage("Ürün açıklaması boş olamaz.!");
+		}
+	}
+}
