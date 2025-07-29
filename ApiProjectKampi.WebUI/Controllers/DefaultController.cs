b431e6fedf24684828b427442c8659000b21e72f
metndurn <mtndrn123@gmail.com>
2025-07-30 00:19:33 +0300

    UI Projesinin Oluşturulması

---
+++ ApiProjectKampi.WebUI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjectKampi.WebUI.Controllers
+{
+	public class DefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ ApiProjectKampi.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiProjectKampi.WebUI.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
